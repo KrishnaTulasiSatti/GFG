5f383774e1507be9fb9efb18c8a78bdfe4e66b12
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-06-23 07:01:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.cpp
+++ Difficulty: Medium/Minimum sum/minimum-sum.cpp
@@ -1,118 +1,61 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
+  
+ string add2num(string a,string b) {
+     
+     reverse(a.begin(),a.end());
+     reverse(b.begin(),b.end());
+     
+     int carry = 0;
+     
+     int i = 0,j =0 ;
+     
+     string res = "";
+     
+   
+     while(i < a.size() || j < b.size() || carry) {
+         int sum = 0;
+         
+         if(i < a.size()) sum += a[i++]-'0';
+         if(j < b.size()) sum += b[j++]-'0';
+         
+         sum += carry;
+         
+       
+         
+         carry = sum/10;
+         
+         res += (sum % 10)+'0';
+         
+         
+         
+     }
+     
+     reverse(res.begin(),res.end());
+     
+     return res;
+     
+     
+ }
     string minSum(vector<int> &arr) {
         // code here
         sort(arr.begin(),arr.end());
+        
         string s1,s2;
+        
+        for(int i = 0 ; i < arr.size() ; i++) {
+           if(i % 2 == 0) s1 += (arr[i]+'0');
+           else s2 += (arr[i]+'0');
+        }
+        
+        string res = add2num(s1,s2);
+        
         int i = 0;
-        while(i < arr.size()) {
-            if(i % 2) s2+=(to_string(arr[i]));
-            else s1+=(to_string(arr[i]));
-            i++;
-        }
-        string ans;
-       // cout<<s1<<endl;
-      //  cout<<s2<<endl;
-        reverse(s1.begin(),s1.end());
-        reverse(s2.begin(),s2.end());
-        i = 0;
-        int j = 0;
-        int carry = 0;
-        while(i < s1.size() && j < s2.size()) {
-            int s = (s1[i]-'0')+(s2[j]-'0')+carry;
-           // cout<<s<<endl;
-            if(s < 10) {
-                ans+=(to_string(s));
-                carry = 0;
-            }
-            else {
-                int a = s % 10;
-                int b = s/10;
-                carry = b;
-                ans+=(to_string(a));
-            }
-            i++;
-            j++;
-            
-            
-        }
         
-        while(i < s1.size()) {
-            int a = (s1[i]-'0')+carry;
-            if(a < 10) {
-                ans+=(to_string(a));
-                carry = 0;
-            }
-            
-            else{
-                int x = a % 10;
-                int y = a / 10;
-                carry = y;
-                ans+=(to_string(x));
-            }
-            i++;
-        }
+        while(res[i] == '0') i++;
         
-        while(j < s2.size()) {
-            int a = (s2[j]-'0')+carry;
-            if(a < 10) {
-                ans+=(to_string(a));
-                carry = 0;
-            }
-            
-            else{
-                int x = a % 10;
-                int y = a / 10;
-                carry =y;
-                ans+=(to_string(x));
-            }
-            j++;
-        }
-        ans+=(to_string(carry));
-        reverse(ans.begin(),ans.end());
-     //   cout<<ans<<endl;
+        res = res.substr(i);
         
-        i = 0;
-        
-        string res = "";
-        while(ans[i] == '0') {
-            i++;
-        }
-        for(int j = i ; j < ans.size() ; j++) res+=ans[j];
         return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution ob;
-        string ans = ob.minSum(a);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
