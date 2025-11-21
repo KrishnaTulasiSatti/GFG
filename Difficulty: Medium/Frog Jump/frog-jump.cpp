5dc455caa389779d91f5cbf0ecaceefbc85f5dbb
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-11-21 18:10:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,30 +1,41 @@
 class Solution {
   public:
+  
   vector<int>dp;
   
   int fun(vector<int>&height,int i,int n) {
-     if(i == n-1) return 0;
-     
-     if(i >= n) return INT_MAX;
-     
-     if(dp[i] != -1) return dp[i];
-     
-     int onestep = fun(height,i+1,n)+abs(height[i]-height[i+1]);
-     
-     int twostep = INT_MAX;
-     
-     if(i+2 < n) {
-         twostep = fun(height,i+2,n)+abs(height[i]-height[i+2]);
-     }
-     
-     return dp[i] = min(onestep,twostep);
-     
+      
+      if(i == n-1) return 0;
+      
+      if(i >= n) return INT_MAX;
+      
+      if(dp[i] != -1) return dp[i];
+      
+      int step1 = INT_MAX;
+      int step2 = INT_MAX;
+      
+      if(i+1 < n) {
+          step1 = abs(height[i]-height[i+1])+fun(height,i+1,n);
+      }
+      
+      if(i+2 < n) {
+          step2 = abs(height[i]-height[i+2])+fun(height,i+2,n);
+      }
+      
+   //   cout << step1 << " " << step2 << endl;
+      
+      return dp[i] = min(step1,step2);
+      
   }
     int minCost(vector<int>& height) {
         // Code here
-        dp.resize(height.size(),-1);
         
-        return fun(height,0,height.size());
+        int n = height.size();
         
+        dp.resize(n,-1);
+        
+        int ans = fun(height,0,n);
+        
+        return ans;
     }
 };
