61225be3251a11699fe6867585d785f22c70fadd
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-12-22 00:06:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -1,48 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function template for C++
 class Solution {
   public:
-    int rowWithMax1s(vector<vector<int> > &arr) {
+    int rowWithMax1s(vector<vector<int>> &arr) {
         // code here
-        int ans = 0;
-        int ind = -1;
+        
+        int ans = -1;
+        int maxi = 0;
+        
         for(int i = 0 ; i < arr.size() ; i++) {
-            int sum = 0;
-            for(int j = 0 ; j < arr[i].size() ; j++) {
-                sum+=arr[i][j];
-            }
-            if(sum > ans) {
-                ans = sum;
-                ind = i;
+            
+            int lb = lower_bound(arr[i].begin(),arr[i].end(),1)-arr[i].begin();
+            int ub = upper_bound(arr[i].begin(),arr[i].end(),1)-arr[i].begin();
+            
+            if(ub-lb > maxi) {
+                maxi = ub-lb;
+                ans = i;
             }
         }
-        return ind;
+        
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int> > arr(n, vector<int>(m));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> arr[i][j];
-            }
-        }
-        Solution ob;
-        auto ans = ob.rowWithMax1s(arr);
-        cout << ans << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
