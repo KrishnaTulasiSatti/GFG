76f73d4f540008724c2d14fdbb588cd650ac9bd0
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-12-26 19:59:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.cpp
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.cpp
@@ -1,65 +1,26 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
-  public:
+public:
     int kthMissing(vector<int> &arr, int k) {
-        // Your code goes here
-        
-        int cnt = 0;
-        int res = 1;
-        while(cnt < k) {
-            if(!binary_search(arr.begin(),arr.end(),res)) {
-                cnt++;
-            }
+        int low = 0, high = arr.size() - 1;
+        int ans = arr.size(); 
+
+        while (low <= high) {
             
-            if(cnt == k) break;
-            res++;
+            int mid = (low + high) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing >= k) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-        return res;
-        
+
+        if (ans < arr.size()) {
+            return k + ans;
+        }
+
+        return arr.back() + (k - (arr.back() - arr.size()));
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        int ans = ob.kthMissing(arr, d);
-        cout << ans << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
