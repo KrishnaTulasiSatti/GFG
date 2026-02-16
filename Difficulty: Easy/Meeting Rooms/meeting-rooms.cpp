83b3e97afe90f3d0169b4ce7f1e735ea5b69a602
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-02-16 19:29:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Code Here
+        sort(arr.begin(),arr.end());
+        
+        int start = arr[0][0];
+        int end = arr[0][1];
+        
+        for(int i = 1 ; i < arr.size() ; i++) {
+            
+            int s = arr[i][0];
+            int e = arr[i][1];
+            
+            if(s < end) return false;
+            end = e;
+        }
+        
+        return true;
+    }
+};
