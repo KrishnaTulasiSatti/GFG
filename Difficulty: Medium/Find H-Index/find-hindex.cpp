78eb1408114f72d0568bd6520986c9262b2a6b49
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-02-21 21:51:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,37 +1,19 @@
 class Solution {
   public:
-  
-  int cnt(int x,vector<int>&citations) {
-      int c = 0;
-      for(auto it : citations) {
-          if(it >= x) c++;
-      }
-      
-      return c;
-  }
     int hIndex(vector<int>& citations) {
         // code here
         
-        int low = 0;
-        int high = *max_element(citations.begin(),citations.end());
+        sort(citations.begin(),citations.end());
         
-        int maxi = 0;
+        int n = citations.size();
         
-        while(low <= high) {
-            int mid = (low+high)/2;
+        for(int i = citations[n-1] ; i >= 0 ; i--) {
             
-            int ans = cnt(mid,citations);
+            int lb = n-(lower_bound(citations.begin(),citations.end(),i)-citations.begin());
             
-            if(ans >= mid) {
-                maxi = max(maxi,mid);
-                low = mid+1;
-            }
-            else {
-                high = mid-1;
-            }
+            if(lb >= i) return i;
+            
         }
-        
-        return maxi;
-        
+        return 0;
     }
 };
