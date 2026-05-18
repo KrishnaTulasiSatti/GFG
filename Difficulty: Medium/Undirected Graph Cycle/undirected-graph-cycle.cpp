ae2775b9727fc25d4d5b30308d7d53361d48ea21
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-18 20:30:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -3,7 +3,8 @@ class Solution {
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
         
-        int n = edges.size();
+        
+        vector<int>vis(V,-1);
         
         vector<vector<int>>adj(V);
         
@@ -13,40 +14,37 @@ class Solution {
             
             adj[u].push_back(v);
             adj[v].push_back(u);
+            
         }
         
-        vector<int>vis(V,-2);
         
         for(int i = 0 ; i < V ; i++) {
-            
-            if(vis[i] == -2) {
-                
-                // cout << i << "--->";
-                
+            if(vis[i] == -1) {
                 queue<pair<int,int>>q;
-        
+                
                 q.push({i,-1});
-                vis[i] = -1;
+                vis[i] = 1;
                 
                 while(!q.empty()) {
-                    
                     auto top = q.front();
                     q.pop();
+                    int node = top.first;
+                    int p = top.second;
                     
-                    for(auto it : adj[top.first]) {
-                        
-        
-                        if((vis[it] == -2)) {
-                            vis[it] = top.first;
-                            q.push({it,top.first});
+                    
+                    for(auto it : adj[node]) {
+                        if(vis[it] == -1) {
+                            vis[it] = 1;
+                            q.push({it,node});
                         }
-                        else if(vis[it] > -2 && vis[top.first] != it) return true;
+                        else if(it != p) return true;
                     }
                 }
-                
             }
         }
         
         return false;
+        
+        
     }
 };
