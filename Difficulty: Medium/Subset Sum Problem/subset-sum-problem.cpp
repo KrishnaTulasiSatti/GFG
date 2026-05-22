2f321d70235533e28f48d7e8eb838f243af5ad62
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-22 08:26:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,33 +1,37 @@
 class Solution {
   public:
     
+    int n;
+    
     vector<vector<int>>dp;
     
-    bool fun(vector<int>&arr,int i,int n,int sum,int tar) {
-        
+    bool fun(vector<int>& arr,int i,int sum) {
         
         if(i >= n) {
-            if(sum == tar) return true;
+            if(sum == 0) return true;
             return false;
         }
         
-        if(sum > tar) return false;
+        if(sum < 0) return false;
         
         if(dp[i][sum] != -1) return dp[i][sum];
         
-        
-        bool pick = fun(arr,i+1,n,sum+arr[i],tar);
-        bool not_pick = fun(arr,i+1,n,sum,tar);
+        bool pick = fun(arr,i+1,sum-arr[i]);
+        bool not_pick = fun(arr,i+1,sum);
         
         return dp[i][sum] = pick || not_pick;
     }
+    
     bool isSubsetSum(vector<int>& arr, int sum) {
         // code here
-        int n = arr.size();
+        n = arr.size();
+        
         dp.resize(n,vector<int>(sum+1,-1));
-        bool res = fun(arr,0,n,0,sum);
-
-        return res;
+        
+        bool ans = fun(arr,0,sum);
+        
+        return ans;
+        
         
     }
 };
